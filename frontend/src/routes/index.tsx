bed469465ff8aa30356c0306690e62fb002f5057
ImaKento <baseballno56221@gmail.com>
2025-05-01 14:10:03 +0900

    TodoListの基礎とSidebar

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Login from "@/pages/Login";
 import Signup from "@/pages/Sinup";
-import Home from "@/pages/Home";
+import TodoList from "@/pages/TodoList";
 import { PrivateRoute } from "./PrivateRoute";
 
 export const AppRoutes = () => {
@@ -14,7 +14,7 @@ export const AppRoutes = () => {
                     path="/"
                     element={
                         <PrivateRoute>
-                            <Home />
+                            <TodoList />
                         </PrivateRoute>
                     }
                 />
